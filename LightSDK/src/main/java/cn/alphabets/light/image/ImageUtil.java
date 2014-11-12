f31711b50cb3fc3620b568abf1dd5d8b95c10bfb
fzcs <fzcs@live.cn>
2014-11-12 17:54:44 +0800

    增加图片缩小，图片上传功能

---
+++ LightSDK/src/main/java/cn/alphabets/light/image/ImageUtil.java
@@ -0,0 +1,83 @@
+package cn.alphabets.light.image;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.RectF;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by 罗浩 on 14/11/12.
+ */
+public class ImageUtil {
+    public static Bitmap ScaleFileBitmapToSize(String pathOfInputImage, int dstWidth, int dstHeight) throws IOException {
+        int inWidth;
+        int inHeight;
+        Log.d("AAABBB", pathOfInputImage);
+        InputStream in = new FileInputStream(pathOfInputImage);
+
+        // decode image size (decode metadata only, not the whole image)
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(in, null, options);
+        in.close();
+
+        // save width and height
+        inWidth = options.outWidth;
+        inHeight = options.outHeight;
+
+        // decode full image pre-resized
+        in = new FileInputStream(pathOfInputImage);
+        options = new BitmapFactory.Options();
+        // calc rought re-size (this is no exact resize)
+        options.inSampleSize = Math.max(inWidth / dstWidth, inHeight / dstHeight);
+        // decode full image
+        Bitmap roughBitmap = BitmapFactory.decodeStream(in, null, options);
+        in.close();
+
+        // calc exact destination size
+        Matrix m = new Matrix();
+        RectF inRect = new RectF(0, 0, roughBitmap.getWidth(), roughBitmap.getHeight());
+        Log.d("AAABBB", inRect.toShortString());
+        RectF outRect = new RectF(0, 0, dstWidth, dstHeight);
+        Log.d("AAABBB", outRect.toShortString());
+        m.setRectToRect(inRect, outRect, Matrix.ScaleToFit.CENTER);
+        float[] values = new float[9];
+        m.getValues(values);
+        Log.d("AAABBB", values.toString());
+        // resize bitmap
+        Bitmap resizedBitmap = Bitmap.createScaledBitmap(roughBitmap, (int) (roughBitmap.getWidth() * values[0]), (int) (roughBitmap.getHeight() * values[4]), true);
+        Log.d("AAABBB", resizedBitmap.getWidth() + "," + resizedBitmap.getHeight());
+        // save image
+
+//                FileOutputStream out = new FileOutputStream(pathOfOutputImage);
+//                resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 80, out);
+        return resizedBitmap;
+
+
+    }
+
+    public static String RealPhotoPath(Uri originalUri, Context context) {
+        //获取照片的实际地址并保存，通知adapter
+        String[] projection = {MediaStore.Images.Media.DATA};
+        Cursor cursor = context.getContentResolver().query(originalUri, projection, null, null, null);
+        if (cursor != null) {
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            String path = cursor.getString(column_index);
+            cursor.close();
+            return path;
+        } else {
+            return null;
+        }
+    }
+}
+
--- LightSDK/src/main/java/cn/alphabets/light/network/NetworkActivity.java
+++ LightSDK/src/main/java/cn/alphabets/light/network/NetworkActivity.java
@@ -1,14 +1,31 @@
 package cn.alphabets.light.network;
 
 import android.app.Activity;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.util.Log;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.Volley;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.net.FileNameMap;
+import java.net.URLConnection;
 import java.util.Map;
 
 /**
@@ -78,6 +95,84 @@ public class NetworkActivity extends Activity {
         if (queue != null) {
             queue.cancelAll(this);
         }
+    }
+
+    public void UploadFile(final File file, final SuccessHandler successHandler, final ErrorHandler errorHandler) {
+
+        class ResponseHolder {
+            ResponseHolder(JSONObject json, Exception exception) {
+                this.json = json;
+                this.exception = exception;
+            }
+
+            JSONObject json;
+            Exception exception;
+        }
+
+        new AsyncTask<File, Integer, ResponseHolder>() {
+
+            @Override
+            protected void onPostExecute(ResponseHolder responseHolder) {
+                super.onPostExecute(responseHolder);
+
+                if (responseHolder.exception != null) {
+                    errorHandler.onError(new VolleyError(responseHolder.exception));
+                } else {
+                    Log.d("AAABBB", responseHolder.json.toString());
+                    successHandler.onSuccess(responseHolder.json);
+                }
+            }
+
+            @Override
+            protected ResponseHolder doInBackground(File... files) {
+
+                HttpClient client = new DefaultHttpClient();
+                Uri.Builder builder = new Uri.Builder();
+                builder.scheme(SessionManager.Scheme).encodedAuthority(SessionManager.Host + ":" + SessionManager.Port).appendEncodedPath("file/create");
+
+
+                //csrf 统一放到url中传给后台
+                if (SessionManager.getCSRFString(NetworkActivity.this) != null) {
+                    builder.appendQueryParameter("_csrf", SessionManager.getCSRFString(NetworkActivity.this));
+                }
+                String url = builder.build().toString();
+                HttpPost post = new HttpPost(url);
+                post.setHeader("Cookie", SessionManager.getCookieString(NetworkActivity.this));
+
+                MultipartEntityBuilder entityBuilder = MultipartEntityBuilder.create();
+
+                for (File file : files) {
+                    if (file.exists()) {
+                        entityBuilder.addBinaryBody("files", file, ContentType.create(GetMimeType(file)), file.getName());
+                    }
+                }
+
+
+                HttpEntity entity = entityBuilder.build();
+                post.setEntity(entity);
+                JSONObject jsonObject = null;
+                Exception exception = null;
+                try {
+                    HttpResponse response = client.execute(post);
+                    int statusCode = response.getStatusLine().getStatusCode();
+                    if (statusCode != HttpStatus.SC_OK) {
+                        throw new HttpResponseException(statusCode, "Error status : " + response.getStatusLine().toString());
+                    } else {
+                        jsonObject = new JSONObject(EntityUtils.toString(response.getEntity(), HTTP.UTF_8));
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    exception = e;
+                }
+                return new ResponseHolder(jsonObject, exception);
+            }
+        }.execute(file);
+    }
+
+    private static String GetMimeType(File file) {
+        FileNameMap fileNameMap = URLConnection.getFileNameMap();
+        String type = fileNameMap.getContentTypeFor(file.getAbsolutePath());
+        return type;
     }
 
     public interface ErrorHandler {
