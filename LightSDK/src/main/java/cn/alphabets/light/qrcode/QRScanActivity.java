87aa58eede6462bfc98412493264c3801a819db7
fzcs <fzcs@live.cn>
2014-11-13 20:12:45 +0800

    删除无用代码

--- LightSDK/src/main/java/cn/alphabets/light/qrcode/QRScanActivity.java
+++ LightSDK/src/main/java/cn/alphabets/light/qrcode/QRScanActivity.java
@@ -155,29 +155,6 @@ public class QRScanActivity extends Activity implements Callback {
 
     }
 
-//    private void initBeepSound() {
-//        if (playBeep && mediaPlayer == null) {
-//            // The volume on STREAM_SYSTEM is not adjustable, and users found it
-//            // too loud,
-//            // so we now play on the music stream.
-//            setVolumeControlStream(AudioManager.STREAM_MUSIC);
-//            mediaPlayer = new MediaPlayer();
-//            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-//            mediaPlayer.setOnCompletionListener(beepListener);
-//
-//            AssetFileDescriptor file = getResources().openRawResourceFd(
-//                    R.raw.beep);
-//            try {
-//                mediaPlayer.setDataSource(file.getFileDescriptor(),
-//                        file.getStartOffset(), file.getLength());
-//                file.close();
-//                mediaPlayer.setVolume(BEEP_VOLUME, BEEP_VOLUME);
-//                mediaPlayer.prepare();
-//            } catch (IOException e) {
-//                mediaPlayer = null;
-//            }
-//        }
-//    }
 
     private static final long VIBRATE_DURATION = 200L;
 
@@ -188,13 +165,6 @@ public class QRScanActivity extends Activity implements Callback {
         }
     }
 
-    /**
-     * When the beep has finished playing, rewind to queue up another one.
-     */
-    private final OnCompletionListener beepListener = new OnCompletionListener() {
-        public void onCompletion(MediaPlayer mediaPlayer) {
-            mediaPlayer.seekTo(0);
-        }
-    };
+
 
 }
