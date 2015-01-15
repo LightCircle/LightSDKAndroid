bd3aa7f3a190f5b81062e036df72b20f5f5bc125
LI LIN <r2space@gmail.com>
2015-01-15 12:08:34 +0800

    添加actionsheet功能

--- LightSDK/src/main/java/cn/alphabets/light/ui/Dialog.java
+++ LightSDK/src/main/java/cn/alphabets/light/ui/Dialog.java
@@ -7,20 +7,26 @@ import android.app.Fragment;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import cn.alphabets.light.R;
 import cn.alphabets.light.exception.ApplicationException;
@@ -134,67 +140,96 @@ public class Dialog {
         dialog.show();
     }
 
+    private static int pixel(Activity activity, int dip){
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP
+                , dip
+                , activity.getResources().getDisplayMetrics());
+    }
 
-//    /**
-//     * 拍摄或选择照片，调用端需要使用onActivityResult来获取图片
-//     * @param context Activity或Fragment
-//     * @param msg 对话框消息
-//     */
-//    public static void takePhoto(final Object context, String msg, final int requestCode) {
-//
-//        Activity activity = context instanceof Fragment ? ((Fragment) context).getActivity() : (Activity) context;
-//        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-//
-//        builder.setMessage(msg);
-//        builder.setPositiveButton("拍照", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//
-//                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//                intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
-//                // File file = FileUtil.getTemporaryFile();
-//                // intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
-//
-//                if (context instanceof  Fragment) {
-//                    ((Fragment) context).startActivityForResult(intent, requestCode);
-//                }
-//                if (context instanceof  Activity) {
-//                    ((Activity) context).startActivityForResult(intent, requestCode);
-//                }
-//                dialog.dismiss();
-//            }
-//        });
-//
-//        builder.setNeutralButton("选择", new DialogInterface.OnClickListener() {
-//            public void onClick(DialogInterface dialog, int id) {
-//
-//                Intent intent = new Intent(Intent.ACTION_PICK);
-//                intent.setType("image/*");
-//
-//                if (context instanceof  Fragment) {
-//                    ((Fragment) context).startActivityForResult(intent, requestCode);
-//                }
-//                if (context instanceof  Activity) {
-//                    ((Activity) context).startActivityForResult(intent, requestCode);
-//                }
-//                dialog.dismiss();
-//            }
-//        });
-//
-//        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
-//            public void onClick(DialogInterface dialog, int id) {
-//                dialog.dismiss();
-//            }
-//        });
-//
-//        AlertDialog dialog = builder.create();
-//        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-//        dialog.show();
-//    }
+    /**
+     * 类似与IOS的ActionSheet，多个按钮选择框
+     * @param context activity or fragment
+     * @param buttons 按钮
+     * @param listener 按钮事件
+     */
+    public static void actionSheet(final Object context, String[] buttons, String cancel, final Click listener) {
 
-//    public static void takePhoto(final Object context, String msg) {
-//        takePhoto(context, msg, Dialog.REQUEST_TAKE_PHOTO);
-//    }
+        Activity activity = context instanceof Fragment ? ((Fragment) context).getActivity() : (Activity) context;
+
+        // 按钮容器
+        View view = activity.getLayoutInflater().inflate(R.layout.dialog_selector, null);
+        LinearLayout layout = (LinearLayout) view.findViewById(R.id.selector_buttons);
+
+        // 弹出window
+        final PopupWindow window = new PopupWindow(view, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        window.setFocusable(true);
+        window.setOutsideTouchable(true);
+
+        // 加上动画并显示
+        window.setAnimationStyle(R.style.PopupAnimation);
+        window.showAtLocation(activity.getWindow().getDecorView().getRootView(), Gravity.BOTTOM, 0, 0);
+
+        // 动态添加按钮
+        for (int i = 0; i < buttons.length; i++) {
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, pixel(activity, 40));
+            params.setMargins(0, pixel(activity, 8), 0, 0);
+
+            TextView button = new TextView(activity);
+            button.setLayoutParams(params);
+            button.setGravity(Gravity.CENTER);
+            button.setText(buttons[i]);
+            button.setBackgroundResource(R.drawable.n_bt_corner);
+            button.setTextColor(activity.getResources().getColor(R.color.blue));
+
+            final int which = i;
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        window.dismiss();
+                        listener.done(which);
+                    }
+                }
+            });
+
+            layout.addView(button);
+        }
+
+        if (cancel != null) {
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, pixel(activity, 40));
+            params.setMargins(0, pixel(activity, 8), 0, 0);
+
+            TextView button = new TextView(activity);
+            button.setLayoutParams(params);
+            button.setGravity(Gravity.CENTER);
+            button.setText(cancel);
+            button.setBackgroundResource(R.drawable.n_bt_corner);
+            button.setTextColor(Color.GRAY);
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        window.dismiss();
+                        listener.done(-1);
+                    }
+                }
+            });
+
+            layout.addView(button);
+        }
+
+        // 点击空白区域或者点击取消则将窗口关闭
+        FrameLayout emptyArea = (FrameLayout) view.findViewById(R.id.selector_area);
+        emptyArea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                window.dismiss();
+            }
+        });
+    }
+
 
     /**
      * 拍摄或选择照片，调用端需要使用onActivityResult来获取图片
