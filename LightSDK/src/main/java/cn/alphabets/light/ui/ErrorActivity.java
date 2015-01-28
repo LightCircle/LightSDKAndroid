f61729a03dd4e93957b96217d2e7e57bc8a9a935
LI LIN <r2space@gmail.com>
2015-01-28 19:37:35 +0800

    错误画面

--- LightSDK/src/main/java/cn/alphabets/light/ui/ErrorActivity.java
+++ LightSDK/src/main/java/cn/alphabets/light/ui/ErrorActivity.java
@@ -1,37 +1,39 @@
 package cn.alphabets.light.ui;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import cn.alphabets.light.R;
+import cn.alphabets.light.application.ABActivity;
+import cn.alphabets.light.setting.Default;
+import cn.alphabets.light.store.Eternal;
 
-public class ErrorActivity extends ActionBarActivity {
+public class ErrorActivity extends ABActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Helper.setNoIconBackActionBar(this, "Error");
         setContentView(R.layout.activity_error);
-    }
 
+        TextView error = (TextView) findViewById(R.id.text_detail);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_error, menu);
-        return true;
+        String lastError = Eternal.getString(Default.LastError);
+        if (lastError != null) {
+            error.setText(lastError);
+        }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        // 返回
+        if (id == android.R.id.home) {
+            onBackPressed();
             return true;
         }
 
