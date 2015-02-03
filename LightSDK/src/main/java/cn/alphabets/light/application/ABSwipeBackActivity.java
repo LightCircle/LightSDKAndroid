4636460854c3a614e55984e82a8926689b9edf82
fzcs <fzcs@live.cn>
2015-02-03 10:47:51 +0800

    完善滑动返回

--- LightSDK/src/main/java/cn/alphabets/light/application/ABSwipeBackActivity.java
+++ LightSDK/src/main/java/cn/alphabets/light/application/ABSwipeBackActivity.java
@@ -1,13 +1,8 @@
 package cn.alphabets.light.application;
 
-import android.app.Activity;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 
-import cn.alphabets.light.R;
 import me.imid.swipebacklayout.lib.SwipeBackLayout;
 import me.imid.swipebacklayout.lib.Utils;
 import me.imid.swipebacklayout.lib.app.SwipeBackActivityBase;
@@ -18,12 +13,12 @@ import me.imid.swipebacklayout.lib.app.SwipeBackActivityHelper;
  */
 public class ABSwipeBackActivity extends ABActivity implements SwipeBackActivityBase {
 
-    private ABSwipeBackActivityHelper mHelper;
+    private SwipeBackActivityHelper mHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mHelper = new ABSwipeBackActivityHelper(this);
+        mHelper = new SwipeBackActivityHelper(this);
         mHelper.onActivityCreate();
     }
 
@@ -57,51 +52,4 @@ public class ABSwipeBackActivity extends ABActivity implements SwipeBackActivity
         getSwipeBackLayout().scrollToFinishActivity();
     }
 
-    private class ABSwipeBackActivityHelper {
-
-        private Activity mActivity;
-
-        private SwipeBackLayout mSwipeBackLayout;
-
-        public ABSwipeBackActivityHelper(Activity activity) {
-            mActivity = activity;
-        }
-
-        public void onActivityCreate() {
-            mActivity.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-            mActivity.getWindow().getDecorView().setBackgroundDrawable(null);
-            mSwipeBackLayout = (SwipeBackLayout) LayoutInflater.from(mActivity).inflate(
-                    R.layout.swipeback_layout, null);
-            mSwipeBackLayout.addSwipeListener(new SwipeBackLayout.SwipeListener() {
-                @Override
-                public void onScrollStateChange(int state, float scrollPercent) {
-                }
-
-                @Override
-                public void onEdgeTouch(int edgeFlag) {
-                    Utils.convertActivityToTranslucent(mActivity);
-                }
-
-                @Override
-                public void onScrollOverThreshold() {
-
-                }
-            });
-        }
-
-        public void onPostCreate() {
-            mSwipeBackLayout.attachToActivity(mActivity);
-        }
-
-        public View findViewById(int id) {
-            if (mSwipeBackLayout != null) {
-                return mSwipeBackLayout.findViewById(id);
-            }
-            return null;
-        }
-
-        public SwipeBackLayout getSwipeBackLayout() {
-            return mSwipeBackLayout;
-        }
-    }
 }
